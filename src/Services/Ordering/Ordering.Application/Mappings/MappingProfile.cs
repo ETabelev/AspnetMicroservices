2a2205548e7f5dada1a79443f68e38ae0979a1ae
Elena <elena.tabelev@symend.com>
2021-10-04 10:17:17 -0600

    Checkout order complete

--- src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,5 +1,5 @@
 ﻿using AutoMapper;
-//using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
 //using Ordering.Application.Features.Orders.Commands.UpdateOrder;
 using Ordering.Application.Features.Orders.Queries.GetOrdersList;
 using Ordering.Domain.Entities;
@@ -10,8 +10,8 @@ namespace Ordering.Application.Mappings
     {
         public MappingProfile()
         {
-            CreateMap<Order, OrdersVm>().ReverseMap();
-          //CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+          CreateMap<Order, OrdersVm>().ReverseMap();
+          CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
           //CreateMap<Order, UpdateOrderCommand>().ReverseMap();
         }
     }
